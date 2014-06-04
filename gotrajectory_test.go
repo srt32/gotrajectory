973a132a163291e89dbb90cd28795d1365c574be
Simon Taranto <simon@thoughtbot.com>
2014-06-03 17:50:45 -0700

    WIP, test all story attrs

--- gotrajectory_test.go
+++ gotrajectory_test.go
@@ -2,6 +2,7 @@ package gotrajectory
 
 import (
   "os"
+  "reflect"
   "testing"
 )
 
@@ -33,5 +34,26 @@ func TestGetStories(t *testing.T) {
     t.Error(stories[0].Title)
   } else {
     t.Log("Passed")
+  }
+
+  var storyTests = []struct {
+    expected string
+    attribute string
+  }{
+    {"Whoohoo!  My first trajectory", "Title"},
+  }
+
+  for _, test := range storyTests {
+    attribute := test.attribute
+    //actual := stories[0].attribute
+    typ := reflect.TypeOf(stories[0])
+    field, _ := typ.FieldByName(attribute)
+    actual_attr := stories[0].Call(field)
+
+    if actual_attr != test.expected {
+      t.Fatalf("FAIL: Expected: %s. Got: %s", test.attribute, actual_attr)
+    } else {
+      t.Log("Passed")
+    }
   }
 }
