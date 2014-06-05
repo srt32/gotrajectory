7eb6ca423cf366382d4829f3fd316058c515e770
Simon Taranto <simon@thoughtbot.com>
2014-06-05 09:21:13 -0700

    Run go fmt

--- gotrajectory.go
+++ gotrajectory.go
@@ -1,69 +1,69 @@
 package gotrajectory
 
-import(
-  "errors"
-  "encoding/json"
-  "net/http"
-  "os"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
 )
 
 const (
-  BASEURL = "https://www.apptrajectory.com/api"
+	BASEURL = "https://www.apptrajectory.com/api"
 )
 
 type Story struct {
-  Archived bool `json:"archived"`
-  AssigneeId int `json:"assignee_id"`
-  CommentsCount int `json:"comments_count"`
-  CreatedAt string `json:"created_at"`
-  Deleted bool `json:"deleted"`
-  DesignNeeded bool `json:"design_needed"`
-  DevelopmentNeeded bool `json:"development_needed"`
-  Id int `json:"id"`
-  IterationId int `json:"iteration_id"`
-  Points int `json:"points"`
-  Position int `json:"position"`
-  State string `json:"state"`
-  StateEvents []string `json:"state_events"`
-  TaskType string `json:"task_type"`
-  Title string `json:"title"`
-  UpdatedAt string `json:"updated_at"`
-  UserId int `json:"user_id"`
+	Archived          bool     `json:"archived"`
+	AssigneeId        int      `json:"assignee_id"`
+	CommentsCount     int      `json:"comments_count"`
+	CreatedAt         string   `json:"created_at"`
+	Deleted           bool     `json:"deleted"`
+	DesignNeeded      bool     `json:"design_needed"`
+	DevelopmentNeeded bool     `json:"development_needed"`
+	Id                int      `json:"id"`
+	IterationId       int      `json:"iteration_id"`
+	Points            int      `json:"points"`
+	Position          int      `json:"position"`
+	State             string   `json:"state"`
+	StateEvents       []string `json:"state_events"`
+	TaskType          string   `json:"task_type"`
+	Title             string   `json:"title"`
+	UpdatedAt         string   `json:"updated_at"`
+	UserId            int      `json:"user_id"`
 }
 
 type response struct {
-  Stories []Story
+	Stories []Story
 }
 
 func GetStories(accountName, projectName string) ([]Story, error) {
-  url := StoriesUrl(accountName, projectName)
-  resp, err := http.Get(url)
-  if err != nil {
-    return nil, err
-  }
+	url := StoriesUrl(accountName, projectName)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  if resp.StatusCode != http.StatusOK {
-    return nil, errors.New(resp.Status)
-  }
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
 
-  r := new(response)
-  err = json.NewDecoder(resp.Body).Decode(r)
-  if err != nil {
-    return nil, err
-  }
+	r := new(response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
 
-  return r.Stories, nil
+	return r.Stories, nil
 }
 
 func StoriesUrl(accountName, projectName string) (url string) {
-  apiKey := os.Getenv("TRAJECTORY_API_KEY")
-  projectPath := "/accounts/" + accountName + "/projects/" + projectName
-  endPoint := "/stories"
-  format := ".json"
+	apiKey := os.Getenv("TRAJECTORY_API_KEY")
+	projectPath := "/accounts/" + accountName + "/projects/" + projectName
+	endPoint := "/stories"
+	format := ".json"
 
-  url = BASEURL + "/" + apiKey + projectPath + endPoint + format
+	url = BASEURL + "/" + apiKey + projectPath + endPoint + format
 
-  return url
+	return url
 }
--- gotrajectory_test.go
+++ gotrajectory_test.go
@@ -1,52 +1,52 @@
 package gotrajectory
 
 import (
-  "os"
-  "testing"
+	"os"
+	"testing"
 )
 
 func TestUrlGeneration(t *testing.T) {
-  accountName := "Ralph"
-  projectName := "P"
-  url := StoriesUrl(accountName, projectName)
-  expectedUrl := "https://www.apptrajectory.com/api/" +
-    os.Getenv("TRAJECTORY_API_KEY") +
-    "/accounts/" +
-    accountName +
-    "/projects/" +
-    projectName +
-    "/stories.json"
+	accountName := "Ralph"
+	projectName := "P"
+	url := StoriesUrl(accountName, projectName)
+	expectedUrl := "https://www.apptrajectory.com/api/" +
+		os.Getenv("TRAJECTORY_API_KEY") +
+		"/accounts/" +
+		accountName +
+		"/projects/" +
+		projectName +
+		"/stories.json"
 
-  if url != expectedUrl {
-    t.Error("Failed. URL is: " + url + ". Expected: " + expectedUrl)
-  } else {
-    t.Log("Passed")
-  }
+	if url != expectedUrl {
+		t.Error("Failed. URL is: " + url + ". Expected: " + expectedUrl)
+	} else {
+		t.Log("Passed")
+	}
 }
 
 func TestGetStories(t *testing.T) {
-  accountName := "simon"
-  projectName := "test"
+	accountName := "simon"
+	projectName := "test"
 
-  stories, err := GetStories(accountName, projectName)
+	stories, err := GetStories(accountName, projectName)
 
-  if err != nil {
-    t.Error(err)
-  } else {
-    t.Log("Passed")
-  }
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log("Passed")
+	}
 
-  title := "Whoohoo!  My first trajectory"
-  if err != nil || stories[0].Title != title {
-    t.Error(stories[0].Title)
-  } else {
-    t.Log("Passed")
-  }
+	title := "Whoohoo!  My first trajectory"
+	if err != nil || stories[0].Title != title {
+		t.Error(stories[0].Title)
+	} else {
+		t.Log("Passed")
+	}
 
-  archived := false
-  if err != nil || stories[0].Archived != archived {
-    t.Error(stories[0].Title)
-  } else {
-    t.Log("Passed")
-  }
+	archived := false
+	if err != nil || stories[0].Archived != archived {
+		t.Error(stories[0].Title)
+	} else {
+		t.Log("Passed")
+	}
 }
