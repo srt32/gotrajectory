a49daefacb0598bf55da52bbcd4d6e748cff9240
Simon Taranto <simon@thoughtbot.com>
2014-06-04 17:43:14 -0700

    Complete attributes for a story

--- gotrajectory.go
+++ gotrajectory.go
@@ -12,7 +12,23 @@ const (
 )
 
 type Story struct {
+  Archived bool `json:"archived"`
+  AssigneeId int `json:"assignee_id"`
+  CommentsCount int `json:"comments_count"`
+  CreatedAt string `json:"created_at"`
+  Deleted bool `json:"deleted"`
+  DesignNeeded bool `json:"design_needed"`
+  DevelopmentNeeded bool `json:"development_needed"`
+  Id int `json:"id"`
+  IterationId int `json:"iteration_id"`
+  Points int `json:"points"`
+  Position int `json:"position"`
+  State string `json:"state"`
+  StateEvents []string `json:"state_events"`
+  TaskType string `json:"task_type"`
   Title string `json:"title"`
+  UpdatedAt string `json:"updated_at"`
+  UserId int `json:"user_id"`
 }
 
 type response struct {
--- gotrajectory_test.go
+++ gotrajectory_test.go
@@ -1,9 +1,7 @@
 package gotrajectory
 
 import (
-  "fmt"
   "os"
-  "reflect"
   "testing"
 )
 
@@ -11,7 +9,9 @@ func TestUrlGeneration(t *testing.T) {
   accountName := "Ralph"
   projectName := "P"
   url := StoriesUrl(accountName, projectName)
-  expectedUrl := "https://www.apptrajectory.com/api/" + os.Getenv("TRAJECTORY_API_KEY") + "/accounts/" +
+  expectedUrl := "https://www.apptrajectory.com/api/" +
+    os.Getenv("TRAJECTORY_API_KEY") +
+    "/accounts/" +
     accountName +
     "/projects/" +
     projectName +
@@ -30,34 +30,23 @@ func TestGetStories(t *testing.T) {
 
   stories, err := GetStories(accountName, projectName)
 
-  storyTitle := "Whoohoo!  My first trajectory"
-  if err != nil || stories[0].Title != storyTitle {
+  if err != nil {
+    t.Error(err)
+  } else {
+    t.Log("Passed")
+  }
+
+  title := "Whoohoo!  My first trajectory"
+  if err != nil || stories[0].Title != title {
     t.Error(stories[0].Title)
   } else {
     t.Log("Passed")
   }
 
-  var storyTests = []struct {
-    expected string
-    attribute string
-  }{
-    {"Whoohoo!  My first trajectory", "Title"},
-  }
-
-  for _, test := range storyTests {
-    attribute := test.attribute
-    fmt.Println(attribute)
-    story := stories[0]
-    attributeFunction := reflect.ValueOf(&story).MethodByName("Title")
-    fmt.Println(attributeFunction)
-
-    actual := attributeFunction.Call([]reflect.Value{})
-    fmt.Println(actual)
-
-    //if actual != test.expected {
-    //  t.Fatalf("FAIL: Expected: %s. Got: %s", test.attribute, actual_attr)
-    //} else {
-    //  t.Log("Passed")
-    //}
+  archived := false
+  if err != nil || stories[0].Archived != archived {
+    t.Error(stories[0].Title)
+  } else {
+    t.Log("Passed")
   }
 }
